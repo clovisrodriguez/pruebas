42bb9e91f4b5d7b16684e74850b8403062e02e03
clovis <clovis.rodriguez@viajaengrande.com>
2018-01-09 19:05:21 -0500

    Nuevos ajustes al proyecto de barranquilla

--- carnavalDeBarranquilla/js/parallax.js
+++ carnavalDeBarranquilla/js/parallax.js
@@ -8,6 +8,8 @@ jQuery(document).ready(function(){
 		$('.parallax1').css('margin-top',(0-(scrolled*.25))+'px');
 		$('.parallax2').css('margin-top',(0-(scrolled*.4))+'px');
 		$('.parallax3').css('margin-top',(0-(scrolled*.75))+'px');
+		$('.parallax4').css('margin-bottom',(0-(scrolled*.2)+300)+'px');
+		$('.parallax5').css('margin-bottom',(0-(scrolled*.3)+400)+'px');
 	}
 
  });
